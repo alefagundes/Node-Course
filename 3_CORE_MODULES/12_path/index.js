5cef9f5fae2a2432639401fc072e207e9b89db14
alefagundes <alefagundes2000.af@gmail.com>
2022-03-21 23:59:50 -0300

    Studying core module path

---
+++ 3_CORE_MODULES/12_path/index.js
@@ -0,0 +1,6 @@
+const path = require('path')
+const customPath = 'relatorio/alessandro/relatorio1.pdf'
+
+console.log(path.dirname(customPath))
+console.log(path.basename(customPath))
+console.log(path.extname(customPath))
