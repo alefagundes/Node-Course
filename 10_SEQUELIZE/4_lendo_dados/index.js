9a3308d3241b79e3517494920b8143f2d1af7b84
alefagundes <alefagundes2000.af@gmail.com>
2022-04-01 13:27:24 -0300

    findOne and findAll with ORM

---
+++ 10_SEQUELIZE/4_lendo_dados/index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const conn = require('./db/conn')
+
+const User = require('./models/User')
+ const app = express()
+
+ app.engine('handlebars', exphbs.engine())
+
+ app.set('view engine', 'handlebars')
+
+ app.use(express.static('public'))
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(express.json())
+
+ app.get('/', async(req, res) => {
+    const users = await User.findAll({raw:true})
+    console.log(users)
+    
+     res.render('home', { users: users })
+ })
+
+ app.get('/users/create', (req, res) => {
+     res.render('adduser')
+ })
+
+ app.post('/users/create', async(req, res) => {
+     const name = req.body.name
+     const ocuppation = req.body.ocuppation
+     let newsletter = req.body.newsletter
+
+     if(newsletter === 'on'){
+         newsletter = true
+     }else{
+         newsletter = false
+     }
+     await User.create({name, ocuppation, newsletter})
+     
+     console.log(req.body)
+
+     res.redirect('/')
+
+ })
+
+ conn.sync().then(() => {
+    app.listen(3000, () => {
+        console.log('App rodando!')
+    })
+    
+ }).catch((err) => {
+     console.log(err)
+ })
+ 
+
