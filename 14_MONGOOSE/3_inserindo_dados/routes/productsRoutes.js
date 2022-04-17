c8bc0e9783cf5b727020525e092842679be30275
alefagundes <alefagundes2000.af@gmail.com>
2022-04-17 03:49:27 -0300

    inserting data and show products in view

---
+++ 14_MONGOOSE/3_inserindo_dados/routes/productsRoutes.js
@@ -0,0 +1,15 @@
+const express  = require('express')
+const router = require('express').Router()
+const ProductController = require('../controllers/ProductController')
+
+
+router.get('/create', ProductController.createProduct)
+router.post('/create', ProductController.createProductPost)
+//router.post('/remove/:id', ProductController.removeProduct)
+//router.get('/:id', ProductController.getProduct)
+//router.get('/edit/:id', ProductController.editProduct)
+//router.post('/edit', ProductController.editProductPost)
+router.get('/', ProductController.showProducts)
+
+
+module.exports = router
